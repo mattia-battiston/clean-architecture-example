9c6392cca28b015b56649e03eed5f0596489f994
kaklakariada <kaklakariada@users.noreply.github.com>
2018-05-12 11:05:47 +0200

    Adapt to new spring-boot version

--- acceptance-tests/src/main/java/com/clean/example/endtoend/EndToEndYatspecTest.java
+++ acceptance-tests/src/main/java/com/clean/example/endtoend/EndToEndYatspecTest.java
@@ -3,13 +3,11 @@ package com.clean.example.endtoend;
 import com.clean.example.yatspec.SpringSpecRunner;
 import com.clean.example.yatspec.YatspecTest;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringSpecRunner.class)
-@SpringApplicationConfiguration(classes = {ApplicationConfigurationForEndToEndTests.class, MocksConfigurationForEndToEndTests.class})
-@IntegrationTest
+@SpringBootTest(classes = {ApplicationConfigurationForEndToEndTests.class, MocksConfigurationForEndToEndTests.class})
 @WebAppConfiguration
 public abstract class EndToEndYatspecTest extends YatspecTest {
 
