15a47f682de6c7b43a10ca089f9ebde74fdc3327
kaklakariada <kaklakariada@users.noreply.github.com>
2018-05-12 11:08:44 +0200

    Fix warnings about missing serialVersionUID

--- application/core/src/main/java/com/clean/example/core/usecase/exchange/getcapacity/ExchangeNotFoundException.java
+++ application/core/src/main/java/com/clean/example/core/usecase/exchange/getcapacity/ExchangeNotFoundException.java
@@ -1,4 +1,5 @@
 package com.clean.example.core.usecase.exchange.getcapacity;
 
 public class ExchangeNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
 }
--- application/dataproviders/src/main/java/com/clean/example/dataproviders/network/deviceclient/DeviceConnectionTimeoutException.java
+++ application/dataproviders/src/main/java/com/clean/example/dataproviders/network/deviceclient/DeviceConnectionTimeoutException.java
@@ -1,4 +1,5 @@
 package com.clean.example.dataproviders.network.deviceclient;
 
 public class DeviceConnectionTimeoutException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
 }
