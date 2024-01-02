e65cb882fa8846c420be6dd46ce6208df4031745
Usuario <Usuario@Portatil-bakero>
2024-01-02 09:28:31 +0100

    variable 0;

--- application/configuration/src/main/java/com/clean/example/Application.java
+++ application/configuration/src/main/java/com/clean/example/Application.java
@@ -11,6 +11,8 @@ import org.springframework.context.annotation.Configuration;
 public class Application {
 
     public static void main(String[] args) {
+    	
+    	int variable = 0;
         SpringApplication.run(Application.class, args);
     }
 
